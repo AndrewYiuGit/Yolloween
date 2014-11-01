40baa253224b8dfc57edba632a6fff2752be7d12
Thai Nguyen <thainguyen@umass.edu>
2014-11-01 08:40:55 -0400

    fix syntax bugs

--- routes/results.js
+++ routes/results.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/:upvotes&downvotes', function(req, res) {
 	var differences = abs(req.params.upvotes - req.params.downvotes);
-	if upvotes > downvotes
+	if (upvotes > downvotes)
   		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/checkmark_xl.png' });
   	else
   		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/cross_xl.png' });
