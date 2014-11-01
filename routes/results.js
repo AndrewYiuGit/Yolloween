5e29dcb2b1032951ec694bfa82f552eb784d3cb9
Andrew Yiu <andrewyiu.git@gmail.com>
2014-11-01 14:17:12 -0400

    Resized images and moved the number to the line after

--- routes/results.js
+++ routes/results.js
@@ -6,9 +6,9 @@ var url = require('url');
 router.get('/', function(req, res) {
 	var differences = parseInt(querystring.parse(url.parse(req.url).query).count);
 	if (differences > 0)
-  		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/checkmark_xl.png' });
+  		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/checkmark_m.png' });
   	else
-  		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/cross_xl.png' });
+  		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/cross_m.png' });
 });
 
 module.exports = router;
