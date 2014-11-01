61a911d222c23ae5646699a55bfb5aa80c5d48f5
Andrew Yiu <andrewyiu.git@gmail.com>
2014-11-01 09:00:33 -0400

    Made changes to the displaying logic and fixed a bug in previous versions

--- routes/results.js
+++ routes/results.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:upvotes&:downvotes', function(req, res) {
-	var differences = Math.abs(req.params.upvotes - req.params.downvotes);
-	if (upvotes > downvotes)
+	var differences = req.params.upvotes - req.params.downvotes;
+	if (differences > 0)
   		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/checkmark_xl.png' });
   	else
   		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/cross_xl.png' });
