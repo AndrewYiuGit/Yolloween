73f7423c1438ce428a58ddbf03e359d566fb666d
Andrew Yiu <andrewyiu.git@gmail.com>
2014-11-01 08:54:52 -0400

    Added missing .

--- routes/results.js
+++ routes/results.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:upvotes&:downvotes', function(req, res) {
-	var differences = Mathabs(req.params.upvotes - req.params.downvotes);
+	var differences = Math.abs(req.params.upvotes - req.params.downvotes);
 	if (upvotes > downvotes)
   		res.render('results', { title: 'Yolloween', count: differences, picture_path: '/assets/images/checkmark_xl.png' });
   	else
