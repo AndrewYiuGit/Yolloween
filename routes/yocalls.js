404287593f20fc83b8b52f2f2d29681a1103ff84
Thai Nguyen <thainguyen@umass.edu>
2014-11-01 02:54:33 -0400

    add database handler

--- routes/yocalls.js
+++ routes/yocalls.js
@@ -2,25 +2,31 @@ var express = require('express');
 var router = express.Router();
 var querystring = require('querystring');
 var url  = require('url');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var db = require('./libs/db');
 
 router.get('/yo',function(req,res){
+	var query = querystring.parse(url.parse(req.url).query);
+	var username = query.username;
+	var location = query.location;
+	var rating = db.getRating(location);
 	res.send("Yo Request");
 });
-
 
 router.get('/upvote',function(req,res){
 	var query = querystring.parse(url.parse(req.url).query);
 	var username = query.username;
 	var location = query.location;
+	db.upVote(username, location);
 	res.send("Hi " + username + ", you're at " + location);
 });
 
 
 router.get('/downvote',function(req,res){
 	var query = querystring.parse(url.parse(request.url).query);
-	console.log(query);
+	var username = query.username;
+	var location = query.location;
+	db.downVote(username, location);
+	res.send("Hi " + username + ", you're at " + location);
 });
 
 module.exports = router;
