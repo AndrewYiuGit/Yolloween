29f27569132e0d77aebc0363c3639e7b4514be83
Thai Nguyen <thainguyen@umass.edu>
2014-11-01 04:23:13 -0400

    done upvote

--- libs/db.js
+++ libs/db.js
@@ -1,9 +1,47 @@
+var https = require('https');
 var db = require('monk')(process.env.MONGODB_LINK);
 var users = db.get('users');
-var location = db.get('locations');
+var locations = db.get('locations');
+
+function getAddress(location, callback){
+	location = location.replace(";",",");
+	var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + location + "&key=" + process.env.GOOGLE_API;
+	https.get(url, function(response){
+		var str = '';
+		response.on('data', function (chunk) {
+    		str += chunk;
+  		});
+  		response.on('end', function () {
+  			data = JSON.parse(str);
+    		callback(data.results[0].formatted_address);
+  		});
+	}).on('error', function(e) {
+  		console.log("Got error: " + e.message);
+	});
+}
 
 function upVote(username, location){
-	
+	getAddress(location, function(address){
+		locations.findOne({full_address: address}, function(err, data){
+			if (err){
+				console.log(err);
+			} else if (data) {
+				console.log(username);
+				console.log(data);
+				if (!(username in data.users)){
+					data.users[data.users.length] = username;
+					data.upvote++;	
+					locations.updateById(data._id,data,function(err, data){
+						if (err){
+							console.log(err);
+						}
+					});
+				}
+			} else {
+				console.log("Found no shit");
+			}
+		})
+	});
 }
 
 function downVote(username, location){
