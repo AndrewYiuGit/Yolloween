1388eefa94b0ec8d7da58f30823e892a01ead081
Thai Nguyen <thainguyen@umass.edu>
2014-11-01 05:17:26 -0400

    update yo request

--- routes/yocalls.js
+++ routes/yocalls.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 var querystring = require('querystring');
 var url  = require('url');
 var db = require('../libs/db');
@@ -9,6 +10,38 @@ router.get('/yo',function(req,res){
 	var username = query.username;
 	var location = query.location;
 	var rating = db.getRating(location, function(data){
+		var url = "http://api.justyo.co/yo/";
+		var post_data = querystring.stringify({
+      		'api_token' : process.env.YOLLOWEEN_API,
+      		'username': username,
+      		'link': 
+  			});
+		
+		var post_options = {
+      		host: 'api.justyo.co',
+      		path: '/yo',
+      		port: 80,
+      		method: 'POST',
+      		headers: {
+          		'Content-Type': 'application/x-www-form-urlencoded',
+          		'Content-Length': post_data.length,
+          		'Connection': 'close'
+     			}
+  			};
+
+  		var post_req = http.request(post_options, function(response) {
+     		str = ''
+      		response.on('data', function (chunk) {
+          		str += chunk;
+      		});
+      		response.on('end', function () {
+  				console.log(str);
+  			});	
+  		}).on('error', function(e) {
+  			console.log("Got error: " + e.message);
+		});
+		post_req.write(post_data);
+  		post_req.end();
 		res.send("Hi " + username + ", you're at " + data.full_address + " with " + data.upvote + " upvotes and " + data.downvote + " downvotes ");
 	});
 });
