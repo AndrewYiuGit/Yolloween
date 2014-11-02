3c72f2667a91a54ff52e2fae2c6124015bf0bfee
Austin Chiang <austinscchiang@gmail.com>
2014-11-01 21:24:56 -0400

    finished up on colors

--- public/javascript/candygraph.js
+++ public/javascript/candygraph.js
@@ -1,5 +1,13 @@
 var chart;
 var dataSet = [];
+var color = [
+"#602749",
+"#F6921D",
+"#96323E",
+"#B14623",
+"#130912",
+"#3E1C33"
+];
 
 nv.addGraph(function() {
   chart = nv.models.lineChart()
@@ -16,10 +24,10 @@ nv.addGraph(function() {
   .tickFormat(d3.format('.r'))
   ;
   d3.select('#chart svg')
-      .datum(getData())
-      .transition().duration(500)
-      .call(chart)
-      ;
+  .datum(getData())
+  .transition().duration(500)
+  .call(chart)
+  ;
   nv.utils.windowResize(chart.update);
 
   return chart;
@@ -33,7 +41,7 @@ function updateChart(){
     type: 'GET',
     data: 'street='+street_name.replace(" ","+"),
     success: function(data) {
-
+      console.log(data);
       dataSet.push({
         street_name: street_name,
         data: data});
@@ -48,13 +56,14 @@ function updateChart(){
     }
   });
 }
+
 function getData() {
   var returnSet = [];
   for(var i = 0 ; i < dataSet.length; i ++){
     returnSet.push({
       values: dataSet[i].data,
       key: dataSet[i].street_name,
-      color: '#ff7f0e'});
+      color: color[i]});
   }
   return returnSet;
 }
--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -12,6 +12,7 @@ function initialize() {
 	};
 	map = new google.maps.Map(document.getElementById('map_canvas'),
 		mapOptions);
+	marker = new google.maps.Marker({map: map});
 
 	google.maps.event.addListener(map, 'click', 
 		function(event) {
@@ -61,8 +62,5 @@ google.maps.event.addDomListener(window, 'load', initialize);
 $('#myModal').on('shown.bs.modal', function () {
 	google.maps.event.trigger(map, "resize");
 	map.setCenter(new google.maps.LatLng(41.255158699999996, -72.9931128));
-	marker = new google.maps.Marker({
-		position: map.getCenter(),
-		map: map
-	});
+	marker.setPosition(map.getCenter());
 });
